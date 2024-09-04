9797b0fc245a340f843e16bed9cd04d59191b070
Yuries404 <yurianysester@gmail.com>
2024-09-03 19:13:12 -0500

    agregando arcihvo procesar compras

---
+++ PARCIALES/PARCIAL_1/procesar_compras.php
@@ -0,0 +1,55 @@
+<?php
+include 'funciones_tienda.php';
+
+$precio_producto = [
+    'camisa' => 50,
+    'pantalon' => 70,
+    'zapatos' => 80,
+    'calcetines' => 10,
+    'gorra' => 25
+];
+
+$carrito = [
+    'camisa' => 2,
+    'pantalon' => 1,
+    'zapatos' => 1,
+    'calcetines' => 3,
+    'gorra' => 0
+];
+
+
+$subtotal = 0;
+
+foreach ($carrito as $productos => $cantidad) {
+    if ($cantidad > 0) {
+        $subtotal += $productos[$producto] * $cantidad;
+    }
+}
+
+$descuento = calcular_descuento($subtotal);
+$impuesto = aplicar_impuesto($subtotal);
+$total = calcular_total($subtotal, $descuento, $impuesto);
+
+echo "<h2>Resumen de Compra</h2>";
+echo "<tr><th>Producto</th><th>Cantidad</th><th>Precio Unitario</th><th>Precio Total</th></tr>";
+
+foreach ($carrito as $producto => $cantidad) {
+    if ($cantidad > 0) {
+        $precio_total = $productos[$producto] * $cantidad;
+        echo "<tr>";
+        echo "<td>{$producto}</td>";
+        echo "<td>{$cantidad}</td>";
+        echo "<td>\${$productos[$producto]}</td>";
+        echo "<td>\${$precio_total}</td>";
+        echo "</tr>";
+    }
+}
+
+echo "</table>";
+
+echo "<p><strong>Subtotal:</strong> \${$subtotal}</p>";
+echo "<p><strong>Descuento:</strong> \${$descuento}</p>";
+echo "<p><strong>Impuesto:</strong> \${$impuesto}</p>";
+echo "<p><strong>Total a Pagar:</strong> \${$total}</p>";
+?>
+?>
