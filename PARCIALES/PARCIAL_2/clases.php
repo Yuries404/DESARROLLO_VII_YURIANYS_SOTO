dc53cf06b2b818d36997330fa9dbe3e0e2b78644
Yuries404 <yurianysester@gmail.com>
2024-09-24 18:56:15 -0500

    agregada carpeta parcial_2

---
+++ PARCIALES/PARCIAL_2/clases.php
@@ -0,0 +1,52 @@
+<?php
+// Archivo: clases.php
+
+class Tarea {
+    public $id;
+    public $titulo;
+    public $descripcion;
+    public $estado;
+    public $prioridad;
+    public $fechaCreacion;
+    public $tipo;
+
+    public function __construct($datos) {
+        foreach ($datos as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    // Implementar estos getters
+    // public function getEstado() { }
+    // public function getPrioridad() { }
+}
+
+class GestorTareas {
+    private $tareas = [];
+
+    public function cargarTareas() {
+        $json = file_get_contents('tareas.json');
+        $data = json_decode($json, true);
+        foreach ($data as $tareaData) {
+            $tarea = new Tarea($tareaData);
+            $this->tareas[] = $tarea;
+        }
+        
+        return $this->tareas;
+    }
+
+    public function agregarTareas() {
+
+    }
+     // agregarTarea($tarea)
+    //eliminarTarea($id)
+    //actualizarTarea($tarea)
+    //actualizarEstadoTarea($id, $nuevoEstado)
+    //buscarTareasPorEstado($estado)
+    // listarTareas($filtroEstado = '')
+}
+
+// Implementar:
+// 1. La interfaz Detalle
+// 2. Modificar la clase Tarea para implementar la interfaz Detalle
+// 3. Las clases TareaDesarrollo, TareaDiseno y TareaTesting que hereden de Tarea
